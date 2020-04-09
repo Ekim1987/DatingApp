b7e8fd8f34842da2580e2c0dc4c426b074dd0c32
Michael <Mike1987@Admins-MacBook-Pro.local>
2020-04-09 19:12:43 +0200

    end of section 4

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -7,7 +7,7 @@ namespace DatingApp.API.Dtos
         [Required]
         public string Username { get; set; }
          [Required]
-        // [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters")]
+        // [StringLength(4, ErrorMessage = "The Password was to short.")]  
         public string Password { get; set; }
 
     }
